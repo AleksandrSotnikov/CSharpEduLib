e8c8a1ce935aee42b3be3bba52b16e5c1b6cf1d4
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-25 19:09:42 +0600

    Add Exercise 1: Hello World with StudentSolution stub and NotImplementedException

---
+++ src/CSharpEduLib.Content/Lectures/Module_1_Basics/Lecture_1_1/Exercises/Exercise_1_HelloWorld/StudentSolution.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace CSharpEduLib.Content.Lectures.Module_1_Basics.Lecture_1_1.Exercises.Exercise_1_HelloWorld
+{
+    /// <summary>
+    /// Упражнение 1: Hello World
+    /// 
+    /// Задание: Напишите программу, которая выводит на экран "Привет, мир!"
+    /// 
+    /// Ожидаемый результат:
+    /// Привет, мир!
+    /// 
+    /// Подсказка: Используйте Console.WriteLine() для вывода текста
+    /// </summary>
+    public class StudentSolution
+    {
+        /// <summary>
+        /// Основной метод упражнения
+        /// TODO: Реализуйте вывод "Привет, мир!" на консоль
+        /// </summary>
+        public static void Run()
+        {
+            // TODO: Замените эту строку на свою реализацию
+            throw new NotImplementedException("Реализуйте этот метод");
+        }
+    }
+}
