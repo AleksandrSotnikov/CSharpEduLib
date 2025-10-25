6ee55fad976a9bb1635ce3ea8d7f88138d9c6384
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-25 21:06:29 +0600

    Extend Exercise model with LectureId, OrderIndex, Type, SolutionTemplate, TestFile

--- src/CSharpEduLib.Core/Models/Exercise.cs
+++ src/CSharpEduLib.Core/Models/Exercise.cs
@@ -1,59 +1,27 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpEduLib.Core.Models
 {
     /// <summary>
-    /// Представляет практическое задание
+    /// Представляет практическое задание (расширено для совместимости с сервисами)
     /// </summary>
     public class Exercise
     {
-        /// <summary>
-        /// Уникальный идентификатор
-        /// </summary>
         public string Id { get; set; }
-        
-        /// <summary>
-        /// Название задания
-        /// </summary>
         public string Title { get; set; }
-        
-        /// <summary>
-        /// Описание задания
-        /// </summary>
         public string Description { get; set; }
-        
-        /// <summary>
-        /// Требования к решению
-        /// </summary>
         public string Requirements { get; set; }
-        
-        /// <summary>
-        /// Пример использования
-        /// </summary>
         public string Example { get; set; }
-        
-        /// <summary>
-        /// Максимальное количество баллов за задание
-        /// </summary>
         public int MaxScore { get; set; }
-        
-        /// <summary>
-        /// Сложность задания (1-5)
-        /// </summary>
         public int Difficulty { get; set; }
-        
-        /// <summary>
-        /// Порядковый номер в лекции
-        /// </summary>
         public int Order { get; set; }
-        
-        /// <summary>
-        /// Подсказки для решения
-        /// </summary>
         public List<string> Hints { get; set; } = new List<string>();
+
+        // Новые свойства, ожидаемые сервисами
+        public string LectureId { get; set; }
+        public int OrderIndex { get; set; }
+        public string Type { get; set; }
+        public string SolutionTemplate { get; set; }
+        public string TestFile { get; set; }
     }
 }
