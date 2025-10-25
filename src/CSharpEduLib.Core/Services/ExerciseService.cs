8b3331584d0b531d11f807649148227488541d2b
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-25 10:39:26 +0600

    Создание основных сервисов: LectureService и ExerciseService

---
+++ src/CSharpEduLib.Core/Services/ExerciseService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpEduLib.Core.Models;
+using CSharpEduLib.Core.Validators;
+
+namespace CSharpEduLib.Core.Services
+{
+    /// <summary>
+    /// Сервис для работы с практическими заданиями
+    /// </summary>
+    public class ExerciseService
+    {
+        private readonly ExerciseValidator _validator;
+        private readonly TestExecutor _testExecutor;
+        
+        public ExerciseService()
+        {
+            _validator = new ExerciseValidator();
+            _testExecutor = new TestExecutor();
+        }
+        
+        /// <summary>
+        /// Получить все задания лекции
+        /// </summary>
+        /// <param name="lecture">Лекция</param>
+        /// <returns>Список заданий</returns>
+        public List<Exercise> GetExercisesForLecture(Lecture lecture)
+        {
+            return lecture.Exercises.OrderBy(e => e.Order).ToList();
+        }
+        
+        /// <summary>
+        /// Получить задание по ID
+        /// </summary>
+        /// <param name="exerciseId">Идентификатор задания</param>
+        /// <param name="lectures">Список всех лекций</param>
+        /// <returns>Задание или null</returns>
+        public Exercise GetExerciseById(string exerciseId, List<Lecture> lectures)
+        {
+            return lectures.SelectMany(l => l.Exercises)
+                          .FirstOrDefault(e => e.Id == exerciseId);
+        }
+        
+        /// <summary>
+        /// Выполнить задание с проверкой
+        /// </summary>
+        /// <param name="exercise">Задание</param>
+        /// <param name="studentCode">Код студента</param>
+        /// <returns>Результат выполнения</returns>
+        public TestResult ExecuteExercise(Exercise exercise, string studentCode)
+        {
+            try
+            {
+                // Проверяем синтаксис
+                var validationResult = _validator.ValidateCode(studentCode);
+                if (!validationResult.IsValid)
+                {
+                    return new TestResult
+                    {
+                        Success = false,
+                        Score = 0,
+                        MaxScore = exercise.MaxScore,
+                        PassedTests = 0,
+                        TotalTests = 1,
+                        Errors = validationResult.Errors
+                    };
+                }
+                
+                // Выполняем тесты
+                return _testExecutor.ExecuteTests(exercise.Id, studentCode);
+            }
+            catch (Exception ex)
+            {
+                return new TestResult
+                {
+                    Success = false,
+                    Score = 0,
+                    MaxScore = exercise.MaxScore,
+                    PassedTests = 0,
+                    TotalTests = 1,
+                    Errors = new List<string> { ex.Message }
+                };
+            }
+        }
+        
+        /// <summary>
+        /// Получить задания по сложности
+        /// </summary>
+        /// <param name="lectures">Список лекций</param>
+        /// <param name="difficulty">Уровень сложности</param>
+        /// <returns>Список заданий</returns>
+        public List<Exercise> GetExercisesByDifficulty(List<Lecture> lectures, int difficulty)
+        {
+            return lectures.SelectMany(l => l.Exercises)
+                          .Where(e => e.Difficulty == difficulty)
+                          .ToList();
+        }
+        
+        /// <summary>
+        /// Получить общее количество заданий
+        /// </summary>
+        /// <param name="lectures">Список лекций</param>
+        /// <returns>Общее количество заданий</returns>
+        public int GetTotalExerciseCount(List<Lecture> lectures)
+        {
+            return lectures.Sum(l => l.Exercises.Count);
+        }
+        
+        /// <summary>
+        /// Получить максимально возможный балл
+        /// </summary>
+        /// <param name="lectures">Список лекций</param>
+        /// <returns>Максимальный балл</returns>
+        public int GetMaxPossibleScore(List<Lecture> lectures)
+        {
+            return lectures.Sum(l => l.Exercises.Sum(e => e.MaxScore));
+        }
+    }
+}
---
+++ src/CSharpEduLib.Core/Services/LectureService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpEduLib.Core.Models;
+using CSharpEduLib.Core.Utils;
+
+namespace CSharpEduLib.Core.Services
+{
+    /// <summary>
+    /// Сервис для работы с лекциями
+    /// </summary>
+    public class LectureService
+    {
+        private readonly List<Lecture> _lectures;
+        private readonly JsonLoader _jsonLoader;
+        
+        public LectureService()
+        {
+            _lectures = new List<Lecture>();
+            _jsonLoader = new JsonLoader();
+        }
+        
+        /// <summary>
+        /// Загрузить все лекции
+        /// </summary>
+        public void LoadAllLectures()
+        {
+            // TODO: Реализация загрузки лекций из файлов
+        }
+        
+        /// <summary>
+        /// Получить лекцию по ID
+        /// </summary>
+        /// <param name="id">Идентификатор лекции</param>
+        /// <returns>Лекция или null</returns>
+        public Lecture GetLectureById(string id)
+        {
+            return _lectures.FirstOrDefault(l => l.Id == id);
+        }
+        
+        /// <summary>
+        /// Получить все лекции модуля
+        /// </summary>
+        /// <param name="moduleNumber">Номер модуля</param>
+        /// <returns>Список лекций</returns>
+        public List<Lecture> GetLecturesByModule(int moduleNumber)
+        {
+            return _lectures.Where(l => l.ModuleNumber == moduleNumber)
+                           .OrderBy(l => l.LectureNumber)
+                           .ToList();
+        }
+        
+        /// <summary>
+        /// Получить следующую лекцию
+        /// </summary>
+        /// <param name="currentLectureId">Идентификатор текущей лекции</param>
+        /// <returns>Следующая лекция или null</returns>
+        public Lecture GetNextLecture(string currentLectureId)
+        {
+            var current = GetLectureById(currentLectureId);
+            if (current == null) return null;
+            
+            return _lectures.FirstOrDefault(l => 
+                l.ModuleNumber == current.ModuleNumber && 
+                l.LectureNumber == current.LectureNumber + 1) ??
+                _lectures.FirstOrDefault(l => 
+                    l.ModuleNumber == current.ModuleNumber + 1 && 
+                    l.LectureNumber == 1);
+        }
+        
+        /// <summary>
+        /// Получить предыдущую лекцию
+        /// </summary>
+        /// <param name="currentLectureId">Идентификатор текущей лекции</param>
+        /// <returns>Предыдущая лекция или null</returns>
+        public Lecture GetPreviousLecture(string currentLectureId)
+        {
+            var current = GetLectureById(currentLectureId);
+            if (current == null) return null;
+            
+            return _lectures.FirstOrDefault(l => 
+                l.ModuleNumber == current.ModuleNumber && 
+                l.LectureNumber == current.LectureNumber - 1);
+        }
+        
+        /// <summary>
+        /// Получить все лекции
+        /// </summary>
+        /// <returns>Список всех лекций</returns>
+        public List<Lecture> GetAllLectures()
+        {
+            return _lectures.OrderBy(l => l.ModuleNumber)
+                           .ThenBy(l => l.LectureNumber)
+                           .ToList();
+        }
+        
+        /// <summary>
+        /// Получить общее количество лекций
+        /// </summary>
+        /// <returns>Количество лекций</returns>
+        public int GetTotalLectureCount()
+        {
+            return _lectures.Count;
+        }
+        
+        /// <summary>
+        /// Получить количество модулей
+        /// </summary>
+        /// <returns>Количество модулей</returns>
+        public int GetTotalModuleCount()
+        {
+            return _lectures.GroupBy(l => l.ModuleNumber).Count();
+        }
+    }
+}
