c6075fa3596bab4bfd8229c9486f5c9ade986140
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-25 10:52:50 +0600

    Добавление тестов-заглушек для Лекции 1.1 (Exercises 1 и 2)

---
+++ tests/CSharpEduLib.Exercises.Tests/Module_1/Lecture_1_1/Exercise_1_Test.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace CSharpEduLib.Exercises.Tests.Module_1.Lecture_1_1
+{
+    [TestFixture]
+    public class Exercise_1_Test
+    {
+        [Test]
+        public void Should_Print_Name_And_Surname()
+        {
+            // TODO: реализовать тест, который компилирует и запускает код студента
+            Assert.Pass("Заглушка теста: реализовать динамическую компиляцию и проверку вывода");
+        }
+    }
+}
---
+++ tests/CSharpEduLib.Exercises.Tests/Module_1/Lecture_1_1/Exercise_2_Test.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace CSharpEduLib.Exercises.Tests.Module_1.Lecture_1_1
+{
+    [TestFixture]
+    public class Exercise_2_Test
+    {
+        [Test]
+        public void Should_Print_Sum_2_and_3()
+        {
+            // TODO: реализовать тест проверки вывода суммы 5
+            Assert.Pass("Заглушка теста: реализовать запуск и проверку вывода");
+        }
+    }
+}
