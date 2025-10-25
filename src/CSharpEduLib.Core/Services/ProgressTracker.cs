2593f1c162b13fa5fe893f4211509b48a2039e84
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-25 10:40:44 +0600

    Основные сервисы: TestExecutor и ProgressTracker

---
+++ src/CSharpEduLib.Core/Services/ProgressTracker.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpEduLib.Core.Models;
+using CSharpEduLib.Core.Utils;
+
+namespace CSharpEduLib.Core.Services
+{
+    /// <summary>
+    /// Сервис для отслеживания прогресса студента
+    /// </summary>
+    public class ProgressTracker
+    {
+        private readonly Dictionary<string, StudentProgress> _studentProgress;
+        private readonly JsonLoader _jsonLoader;
+        
+        public ProgressTracker()
+        {
+            _studentProgress = new Dictionary<string, StudentProgress>();
+            _jsonLoader = new JsonLoader();
+        }
+        
+        /// <summary>
+        /// Получить прогресс студента
+        /// </summary>
+        /// <param name="studentId">Идентификатор студента</param>
+        /// <returns>Прогресс студента</returns>
+        public StudentProgress GetProgress(string studentId)
+        {
+            if (!_studentProgress.ContainsKey(studentId))
+            {
+                _studentProgress[studentId] = new StudentProgress
+                {
+                    StudentId = studentId,
+                    CompletedModules = 0,
+                    TotalModules = 6, // По ТЗ
+                    CompletedLectures = 0,
+                    TotalLectures = 30, // По ТЗ
+                    TotalScore = 0,
+                    MaxPossibleScore = 0
+                };
+            }
+            
+            return _studentProgress[studentId];
+        }
+        
+        /// <summary>
+        /// Отметить завершение лекции
+        /// </summary>
+        /// <param name="studentId">Идентификатор студента</param>
+        /// <param name="lecture">Лекция</param>
+        public void MarkLectureCompleted(string studentId, Lecture lecture)
+        {
+            var progress = GetProgress(studentId);
+            progress.CompletedLectures++;
+            
+            // Обновляем прогресс модуля
+            if (!progress.ModuleProgress.ContainsKey(lecture.ModuleNumber))
+            {
+                progress.ModuleProgress[lecture.ModuleNumber] = new ModuleProgress
+                {
+                    ModuleNumber = lecture.ModuleNumber,
+                    ModuleName = $"Модуль {lecture.ModuleNumber}",
+                    CompletedLectures = 0,
+                    TotalLectures = GetLectureCountForModule(lecture.ModuleNumber),
+                    Score = 0,
+                    MaxScore = 0
+                };
+            }
+            
+            progress.ModuleProgress[lecture.ModuleNumber].CompletedLectures++;
+        }
+        
+        /// <summary>
+        /// Обновить скор за выполненное задание
+        /// </summary>
+        /// <param name="studentId">Идентификатор студента</param>
+        /// <param name="moduleNumber">Номер модуля</param>
+        /// <param name="score">Полученные баллы</param>
+        public void UpdateScore(string studentId, int moduleNumber, int score)
+        {
+            var progress = GetProgress(studentId);
+            progress.TotalScore += score;
+            
+            if (progress.ModuleProgress.ContainsKey(moduleNumber))
+            {
+                progress.ModuleProgress[moduleNumber].Score += score;
+            }
+        }
+        
+        /// <summary>
+        /// Проверить, завершен ли модуль
+        /// </summary>
+        /// <param name="studentId">Идентификатор студента</param>
+        /// <param name="moduleNumber">Номер модуля</param>
+        /// <returns>True, если модуль завершен</returns>
+        public bool IsModuleCompleted(string studentId, int moduleNumber)
+        {
+            var progress = GetProgress(studentId);
+            return progress.ModuleProgress.ContainsKey(moduleNumber) &&
+                   progress.ModuleProgress[moduleNumber].IsCompleted;
+        }
+        
+        /// <summary>
+        /// Получить статистику по всем студентам
+        /// </summary>
+        /// <returns>Общая статистика</returns>
+        public Dictionary<string, object> GetOverallStatistics()
+        {
+            var stats = new Dictionary<string, object>
+            {
+                ["TotalStudents"] = _studentProgress.Count,
+                ["AverageCompletionRate"] = _studentProgress.Values.Average(p => p.CompletionPercentage),
+                ["TotalExercisesCompleted"] = _studentProgress.Values.Sum(p => p.CompletedLectures),
+                ["AverageScore"] = _studentProgress.Values.Average(p => p.TotalScore)
+            };
+            
+            return stats;
+        }
+        
+        /// <summary>
+        /// Сохранить прогресс в файл
+        /// </summary>
+        /// <param name="filePath">Путь к файлу</param>
+        public void SaveProgress(string filePath)
+        {
+            // TODO: Реализация сохранения в JSON
+        }
+        
+        /// <summary>
+        /// Загрузить прогресс из файла
+        /// </summary>
+        /// <param name="filePath">Путь к файлу</param>
+        public void LoadProgress(string filePath)
+        {
+            // TODO: Реализация загрузки из JSON
+        }
+        
+        /// <summary>
+        /// Получить количество лекций в модуле
+        /// </summary>
+        /// <param name="moduleNumber">Номер модуля</param>
+        /// <returns>Количество лекций</returns>
+        private int GetLectureCountForModule(int moduleNumber)
+        {
+            // TODO: Получать реальное количество из конфигурации
+            return 5; // Пока что по умолчанию
+        }
+    }
+}
---
+++ src/CSharpEduLib.Core/Services/TestExecutor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using CSharpEduLib.Core.Models;
+
+namespace CSharpEduLib.Core.Services
+{
+    /// <summary>
+    /// Сервис для выполнения unit-тестов
+    /// </summary>
+    public class TestExecutor
+    {
+        /// <summary>
+        /// Выполнить тесты для задания
+        /// </summary>
+        /// <param name="exerciseId">Идентификатор задания</param>
+        /// <param name="studentCode">Код студента</param>
+        /// <returns>Результат выполнения тестов</returns>
+        public TestResult ExecuteTests(string exerciseId, string studentCode)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var testResult = new TestResult
+            {
+                TestDetails = new List<TestDetail>()
+            };
+            
+            try
+            {
+                // TODO: Реализация динамической компиляции и выполнения кода
+                // TODO: Загрузка соответствующих unit-тестов
+                // TODO: Выполнение NUnit тестов
+                
+                // Пока что заглушка
+                testResult.Success = true;
+                testResult.Score = 10;
+                testResult.MaxScore = 10;
+                testResult.PassedTests = 1;
+                testResult.TotalTests = 1;
+                
+                testResult.TestDetails.Add(new TestDetail
+                {
+                    TestName = "Sample Test",
+                    Passed = true,
+                    ErrorMessage = null,
+                    ExecutionTimeMs = 50
+                });
+            }
+            catch (Exception ex)
+            {
+                testResult.Success = false;
+                testResult.Score = 0;
+                testResult.Errors.Add($"Ошибка выполнения: {ex.Message}");
+            }
+            finally
+            {
+                stopwatch.Stop();
+                testResult.ExecutionTimeMs = stopwatch.ElapsedMilliseconds;
+            }
+            
+            return testResult;
+        }
+        
+        /// <summary>
+        /// Получить список доступных тестов для задания
+        /// </summary>
+        /// <param name="exerciseId">Идентификатор задания</param>
+        /// <returns>Список имен тестов</returns>
+        public List<string> GetAvailableTests(string exerciseId)
+        {
+            // TODO: Реализация получения списка тестов
+            return new List<string> { "Sample Test" };
+        }
+        
+        /// <summary>
+        /// Проверить, существуют ли тесты для задания
+        /// </summary>
+        /// <param name="exerciseId">Идентификатор задания</param>
+        /// <returns>True, если тесты существуют</returns>
+        public bool HasTestsForExercise(string exerciseId)
+        {
+            // TODO: Реализация проверки наличия тестов
+            return true;
+        }
+    }
+}
