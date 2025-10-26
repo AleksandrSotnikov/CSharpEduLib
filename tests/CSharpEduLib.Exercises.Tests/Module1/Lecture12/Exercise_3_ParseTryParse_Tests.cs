ab6c7b8074d20d5b24a1dab8297506c4cf8e27f1
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-26 08:09:19 +0300

    test(module-1): add initial NUnit test stubs for Lecture 1.2 exercises (1-3)

---
+++ tests/CSharpEduLib.Exercises.Tests/Module1/Lecture12/Exercise_3_ParseTryParse_Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace CSharpEduLib.Exercises.Tests.Module1.Lecture12
+{
+    [TestFixture]
+    public class Exercise_3_ParseTryParse_Tests
+    {
+        [Test]
+        public void ShouldParseAndTryParse()
+        {
+            Assert.Pass();
+        }
+    }
+}
