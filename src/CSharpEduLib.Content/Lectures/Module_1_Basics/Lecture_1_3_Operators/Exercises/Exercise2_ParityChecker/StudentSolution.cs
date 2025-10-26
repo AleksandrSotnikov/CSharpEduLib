a5d70db8320d1a01165f470349cf9d4b6df21d80
Aleksandr Sotnikov <67824940+AleksandrSotnikov@users.noreply.github.com>
2025-10-26 11:39:26 +0300

    Lecture 1.3: add Exercise2 StudentSolution (ParityChecker)

---
+++ src/CSharpEduLib.Content/Lectures/Module_1_Basics/Lecture_1_3_Operators/Exercises/Exercise2_ParityChecker/StudentSolution.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSharpEduLib.Content.Lectures.Module1.Lecture13.Exercises.Exercise2
+{
+    /// <summary>
+    /// Упражнение 2: Определение чётности
+    /// Вернуть "Even" или "Odd" в зависимости от значения n
+    /// </summary>
+    public class ParityChecker
+    {
+        public string GetParity(int n)
+        {
+            // TODO: используйте оператор % для определения чётности
+            throw new NotImplementedException();
+        }
+    }
+}
